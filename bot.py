256a1f660f6f8f9a2ddb21ed8752aaff568f0c84
Movɩe Vɩɭɭʌ <125588667+MovieVillaYT@users.noreply.github.com>
2023-02-17 12:17:19 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -53,7 +53,7 @@ if __name__ == "__main__" :
         async for user in banned_users:
             temp.BANNED_USERS.append(user["user_id"])
         logging.info(LOG_STR)
-        await broadcast_admins(self, '** Bot started successfully **\n\nBot By @MovieVillaYT)
+        await broadcast_admins(self, '** Bot started successfully **\n\nBot By @MovieVillaYT')
         logging.info('Bot started')
 
 
