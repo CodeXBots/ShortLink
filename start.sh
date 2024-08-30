213abbbf06b4f3beee612342041ae80a99424c5e
Code X Bots <125588667+codexbots@users.noreply.github.com>
2024-08-30 23:14:35 +0530

    Update and rename start.sh to run cmd.txt

--- start.sh
+++
@@ -1,6 +0,0 @@
-echo "Cloning Repo...."
-git clone https://github.com/CodeXBots/Shortner-Converter-Bot.git /Shortner-Converter-Bot
-cd /Shortner-Converter-Bot
-pip3 install -r requirements.txt
-echo "Starting Bot...."
-python3 bot.py
