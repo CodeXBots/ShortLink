1c702b1ae00520e32d4082e331b8b7c822020b8c
Code X Bots <125588667+codexbots@users.noreply.github.com>
2024-08-31 08:33:06 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -1,96 +1,72 @@
-# Don't Edit
-
+from pyrogram.errors import InputUserDeactivated, UserNotParticipant, FloodWait, UserIsBlocked, PeerIdInvalid
+from plugins.dbusers import db
+from pyrogram import Client, filters
+from config import ADMINS
 import asyncio
 import datetime
-import logging
-import random
-import string
 import time
-import traceback
+import logging
 
-import aiofiles
-import aiofiles.os
-from config import *
-from database import *
-from pyrogram import *
-from pyrogram.errors import *
-from pyrogram.types import *
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
-
-broadcast_ids = {}
-
-@Client.on_message(filters.command("broadcast") & filters.private & filters.user(ADMINS))
-async def broadcast_handler(c:Client, m:Message):
-    if m.reply_to_message:
-        try:
-            await main_broadcast_handler(m)
-        except Exception as e:
-            logging.error("Failed to broadcast", exc_info=True)
-    else:
-        await m.reply_text("Reply to the message you want to broadcast")
-
-async def send_msg(user_id, message):
+async def broadcast_messages(user_id, message):
     try:
-        if BROADCAST_AS_COPY == "False":
-            await message.forward(chat_id=user_id)
-        elif BROADCAST_AS_COPY == "True":
-            await message.copy(chat_id=user_id)
-        return 200, None
+        await message.copy(chat_id=user_id)
+        return True, "Success"
     except FloodWait as e:
         await asyncio.sleep(e.x)
-        return send_msg(user_id, message)
+        return await broadcast_messages(user_id, message)
     except InputUserDeactivated:
-        return 400, f"{user_id} : deactivated\n"
+        await db.delete_user(int(user_id))
+        logging.info(f"{user_id}-Removed from Database, since deleted account.")
+        return False, "Deleted"
     except UserIsBlocked:
-        return 400, f"{user_id} : blocked the bot\n"
+        logging.info(f"{user_id} -Blocked the bot.")
+        return False, "Blocked"
     except PeerIdInvalid:
-        return 400, f"{user_id} : user id invalid\n"
+        await db.delete_user(int(user_id))
+        logging.info(f"{user_id} - PeerIdInvalid")
+        return False, "Error"
     except Exception as e:
-        return 500, f"{user_id} : {traceback.format_exc()}\n"
+        return False, "Error"
 
-
-async def main_broadcast_handler(m: Message):
-    all_users = await get_all_users()
-    broadcast_msg = m.reply_to_message
-    while True:
-        broadcast_id = ''.join([random.choice(string.ascii_letters) for _ in range(3)])
-        if not broadcast_ids.get(broadcast_id):
-            break
-    out = await m.reply_text(text="Broadcast Started! You will be notified with log file when all the users are notified.")
-
+@Client.on_message(filters.command("broadcast") & filters.user(ADMINS) & filters.reply)
+async def verupikkals(bot, message):
+    users = await db.get_all_users()
+    b_msg = message.reply_to_message
+    sts = await message.reply_text(
+        text='Broadcasting your messages...'
+    )
     start_time = time.time()
-    total_users = await total_users_count()
+    total_users = await db.total_users_count()
     done = 0
-    failed = 0
+    blocked = 0
+    deleted = 0
+    failed =0
+
     success = 0
-    broadcast_ids[broadcast_id] = dict(total=total_users, current=done, failed=failed, success=success)
-
-    async with aiofiles.open('broadcast.txt', 'w') as broadcast_log_file:
-        async for user in all_users:
-            sts, msg = await send_msg(user_id=int(user['user_id']), message=broadcast_msg)
-            if msg is not None:
-                await broadcast_log_file.write(msg)
-            if sts == 200:
+    async for user in users:
+        if 'id' in user:
+            pti, sh = await broadcast_messages(int(user['id']), b_msg)
+            if pti:
                 success += 1
-            else:
-                failed += 1
-            if sts == 400:
-                await delete_user(user['user_id'])
+            elif pti == False:
+                if sh == "Blocked":
+                    blocked += 1
+                elif sh == "Deleted":
+                    deleted += 1
+                elif sh == "Error":
+                    failed += 1
             done += 1
-            if broadcast_ids.get(broadcast_id) is None:
-                break
-            else:
-                broadcast_ids[broadcast_id].update(dict(current=done, failed=failed, success=success))
-
-    if broadcast_ids.get(broadcast_id):
-        broadcast_ids.pop(broadcast_id)
-    completed_in = datetime.timedelta(seconds=int(time.time() - start_time))
-    await asyncio.sleep(3)
-    await out.delete()
-    if failed == 0:
-        await m.reply_text(text=f"Broadcast completed in `{completed_in}`\n\nTotal users {total_users}.\nTotal done {done}, {success} success and {failed} failed.", quote=True)
-
-    else:
-        await m.reply_document(document='broadcast.txt', caption=f"Broadcast completed in `{completed_in}`\n\nTotal users {total_users}.\nTotal done {done}, {success} success and {failed} failed.", quote=True)
-
-    await aiofiles.os.remove('broadcast.txt')
+            if not done % 20:
+                await sts.edit(f"Broadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")    
+        else:
+            # Handle the case where 'id' key is missing in the user dictionary
+            done += 1
+            failed += 1
+            if not done % 20:
+                await sts.edit(f"Broadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")    
+    
+    time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
+    await sts.edit(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")
