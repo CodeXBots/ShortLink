9ed7847b702189efd7bac3b9e3d30081322ef77c
CodeXBots <RahulReviewsYT@gmail.com>
2024-09-15 07:25:52 +0530

    Update and rename config.py to configs.py

---
+++ configs.py
@@ -0,0 +1,25 @@
+from os import getenv as genv
+
+API_ID = genv("API_ID", "")
+API_HASH = genv("API_HASH", "")
+BOT_TOKEN = genv("BOT_TOKEN", "")
+BASE_URL = genv("BASE_URL", "")
+SUPPORT_GROUP = genv("SUPPORT_GROUP", "CodeXsupport")
+UPDATES_CHANNEL = genv("UPDATES_CHANNEL", "RahulReviewsYT")
+DATABASE_URL = genv("DATABASE_URL", "")
+START_TXT = '''<b>Hᴇʟʟᴏ {}, I Aᴍ ShortLink Convetor Bot.
+๏ I ᴄᴀɴ Cᴏɴᴠᴇʀᴛ ʏᴏᴜʀ ʟɪɴᴋs ᴛᴏ Sʜᴏʀᴛ ʟɪɴᴋs ᴜsɪɴɢ ʏᴏᴜʀ ᴀᴩɪ.
+๏ ᴄʟɪᴄᴋ ᴏɴ ᴛʜᴇ Hᴇʟᴩ Mᴇɴᴜ ʙᴜᴛᴛᴏɴ ʙᴇʟᴏᴡ ᴛᴏ ɢᴇᴛ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ᴀʙᴏᴜᴛ ᴍʏ ᴄᴏᴍᴍᴀɴᴅs.
+๏ Uᴘᴅᴀᴛᴇs - @RahulReviewsYT</b>'''
+HELP_TXT = '''Sᴇɴᴅ ᴍᴇ ᴀɴʏ ʟɪɴᴋ ɪ ᴡɪʟʟ ᴄᴏɴᴠᴇʀᴛ ɪᴛ ᴛᴏ sʜᴏʀᴛ ʟɪɴᴋ ᴜsɪɴɢ ʏᴏᴜʀ ᴀᴘɪ
+ʏᴏᴜ ᴄᴀɴ sʜᴀʀᴇ ᴛʜᴀᴛ ʟɪɴᴋ ᴀɴᴅ ᴇᴀʀɴ ᴍᴏɴᴇʏ.
+sᴇɴᴅ /shortner ᴛᴏ ᴋɴᴏᴡ ᴍᴏʀᴇ - Uᴘᴅᴀᴛᴇs - @RahulReviewsYT'''
+ABOUT_TXT = '''<b>╔════❰ ShortLink Convetor ❱═══❍
+║ ┏━━━━━━━━━❥
+║ ┣ Mʏ ɴᴀᴍᴇ -> {}
+║ ┣ Mʏ Oᴡɴᴇʀ -> @CodeXBro
+║ ┣ Uᴘᴅᴀᴛᴇꜱ -> @RahulreviewsYT
+║ ┣ 𝖲ᴜᴘᴘᴏʀᴛ -> @CodeXsupport
+║ ┣ ๏ Cʜᴇᴄᴋ ʜᴇʟᴘ ᴛᴏ ᴋɴᴏᴡ ᴍᴏʀᴇ.
+║ ┗━━━━━━━━━❥
+╚═════❰ @ ❱═════❍</b>'''
